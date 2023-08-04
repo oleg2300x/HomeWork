db5d2b8a19103fa11fd7c6123755c56d2e120170
oleg2300x <140871416+oleg2300x@users.noreply.github.com>
2023-08-04 21:28:11 +1000

    Update str_func.py
    Fix bags

--- str_func.py
+++ str_func.py
@@ -6,3 +6,6 @@ def FirstLettertoUpper(text):
     """Переводик первую букву слова в верхний регистр"""
     text_ = text.title()
     return text_
+def UperinLower(text):
+    """Функция приводит все буквы в нижний регистр"""
+    return text.lower()
