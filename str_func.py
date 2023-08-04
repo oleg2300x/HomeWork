fadd7f5ce5961eec88008eb4eff4b9b33e1395b5
Oleg <panasenko23o@mail.ru>
2023-08-04 20:52:05 +1000

    add Func Firsr letertouper

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def LowerinUper(text):
+    """Переводит все буквы в верхний регистр"""
     return text.upper()
+
+def FirstLettertoUpper(text):
+    """Переводик первую букву слова в верхний регистр"""
+    text_ = text.title()
+    return text_
